1c858992aa9ac7fa9a829592ab51783421cc2d91
郑巍 <zhengwei@xiaojiuwo.cn>
2017-09-13 18:56:22 +0800

    UI done

--- src/main/resources/static/jsx/index.jsx
+++ src/main/resources/static/jsx/index.jsx
@@ -1,4 +1,6 @@
 import MainPanel from './main/mainPanel.jsx';
+import MainMenu from './menu/main.jsx';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {
   BrowserRouter as Router,
   Route,
@@ -7,26 +9,31 @@ import {
 
 
 const initFunc = function() {
-  const BasicExample = () => (
-  <Router>
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-      </ul>
+//   const BasicExample = () => (
+//   <Router>
+//     <div>
+//       <ul>
+//         <li><Link to="/">Home</Link></li>
+//         <li><Link to="/about">About</Link></li>
+//         <li><Link to="/topics">Topics</Link></li>
+//       </ul>
+//
+//       <hr/>
+//
+//       <Route exact path="/" component={MainPanel}/>
+//       <Route path="/about" component={MainPanel}/>
+//       <Route path="/topics" component={MainPanel}/>
+//     </div>
+//   </Router>
+// )
 
-      <hr/>
-
-      <Route exact path="/" component={MainPanel}/>
-      <Route path="/about" component={MainPanel}/>
-      <Route path="/topics" component={MainPanel}/>
-    </div>
-  </Router>
-)
-
+const App = () => (
+  <MuiThemeProvider>
+    <MainMenu/> 
+  </MuiThemeProvider>
+);
   ReactDOM.render(
-    <BasicExample />, document.getElementById('teststtststsPanel'));
+    <App/>, document.getElementById('teststtststsPanel'));
 }
 
 export default initFunc;
